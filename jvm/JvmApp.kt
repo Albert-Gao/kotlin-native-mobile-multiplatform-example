3f93262c651a65a28fa1e479d848674f28d05650
Albert <albertgaohy@gmail.com>
2018-02-26 12:39:29 +1300

    Add jvm consume example

---
+++ jvm/JvmApp.kt
@@ -0,0 +1,6 @@
+import xyz.albertgao.libs.*
+
+fun main(args: Array<String>) {
+    val sample = Sample()
+    println(sample.checkPlatform())
+}
--- platforms/jvm/main/xyz/albertgao/libs/Platform.kt
+++ platforms/jvm/main/xyz/albertgao/libs/Platform.kt
@@ -4,6 +4,6 @@ actual class Platform actual constructor(
   private val url: String
 ) {
   actual fun get():String {
-    return "KN-Android $url"
+    return "KN-JVM $url"
   }
 }
